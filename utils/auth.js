b4b792dfec3e4a7a9ef62a130a50fbaa08d06f3b
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-05-22 12:12:33 -0500

    Handlebars.login - 8 characters password

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,4 @@
 const withAuth = (req, res, next) => {
-<<<<<<< HEAD
-
-=======
->>>>>>> f0f21e2f9cb79429f6206a955bea717f4a28d905
   if (!req.session.logged_in) {
     res.redirect('/login');
   } else {
